b5c9d811bf737da66b9b5d4f5e0d45b3a917330d
Hernan Montoya <hernan-montoya@hotmail.com>
2022-10-10 22:49:16 -0500

    Primer commit

---
+++ src/main/java/com/project/quadbikes/QuadbikesApplication.java
@@ -0,0 +1,13 @@
+package com.project.quadbikes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuadbikesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuadbikesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/quadbikes/QuadbikesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.quadbikes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuadbikesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
