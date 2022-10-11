18d522611de8a5e2ec7c0b00b54b101f7b4eba36
Hernan Montoya <hernan-montoya@hotmail.com>
2022-10-10 23:23:05 -0500

    Clases del paquete model creadas

--- src/main/java/com/project/quadbikes/model/quadbike.java
+++ src/main/java/com/project/quadbikes/model/quadbike.java
@@ -4,10 +4,73 @@
  */
 package com.project.quadbikes.model;
 
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  *
  * @author hernan
  */
-public class quadbike {
+@Table
+@Entity(name="quadbike")
+public class quadbike implements Serializable{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="id")
+    private Integer id;
+    @Column(name="brand")
+    private String brand;
+    @Column(name="model")
+    private Integer model;
+    @Column(name="category_id")
+    private Integer category_id;
+    @Column(name="name")
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public Integer getModel() {
+        return model;
+    }
+
+    public void setModel(Integer model) {
+        this.model = model;
+    }
+
+    public Integer getCategory_id() {
+        return category_id;
+    }
+
+    public void setCategory_id(Integer category_id) {
+        this.category_id = category_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
     
 }
